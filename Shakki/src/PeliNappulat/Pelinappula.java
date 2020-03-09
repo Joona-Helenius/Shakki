b2c81098328d305413bdc44e14091191ab2b12b4
Atte Meskanen <meskanen_atte@hotmail.com>
2020-03-09 21:30:20 +0200

    Line of sight lisätty

---
+++ Shakki/src/PeliNappulat/Pelinappula.java
@@ -0,0 +1,105 @@
+package PeliNappulat;
+
+import Shakki.*;
+
+public class Pelinappula {
+	protected int omistaja;
+	protected Sijainti sijainti;
+	
+	public Pelinappula(int omistaja, Sijainti sijainti) {
+		this.omistaja = omistaja;
+		this.sijainti = sijainti;
+	}
+	public Sijainti getSijainti() {
+		return this.sijainti;
+	}
+	public void setSijainti(Sijainti sijainti) {
+		this.sijainti = sijainti;
+	}
+	
+	public boolean isPathClear(Sijainti alku, Sijainti loppu, PeliLauta shakkilauta) {
+		if (alku.getRow() == loppu.getRow()) {
+			if (alku.getCol() > loppu.getCol()) {
+				for (int i = alku.getCol() +1; i < loppu.getCol(); i++) {
+					if (shakkilauta.isPieceAt(alku.getCol(), i)) {
+						return false;
+					}
+				}
+			}
+			if (alku.getCol() < loppu.getCol()) {
+				for (int i = alku.getCol() -1; i> loppu.getCol(); i--) {
+					if (shakkilauta.isPieceAt(alku.getCol(), i)) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+		if (alku.getCol() == loppu.getCol()) {
+			if (alku.getRow() > loppu.getRow()) {
+				for (int i = alku.getRow() +1; i < loppu.getRow(); i++) {
+					if (shakkilauta.isPieceAt(alku.getRow(), i)) {
+						return false;
+					}
+				}
+			}
+			if (alku.getRow() < loppu.getRow()) {
+				for (int i = alku.getRow(); i < loppu.getRow(); i--) {
+					if (shakkilauta.isPieceAt(alku.getRow(), i)) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+		if (alku.getCol() - loppu.getCol() == alku.getRow() - loppu.getRow()) {
+			if (alku.getRow() < loppu.getRow() && alku.getCol() < loppu.getCol()) {
+				int counter = 1;
+				for (int i = alku.getRow(); i< loppu.getRow(); i++) {
+					if (shakkilauta.isPieceAt(alku.getRow()+counter, alku.getCol()+counter)) {
+						return false;
+					}
+					counter++;
+				}
+			}
+			if (alku.getRow() > loppu.getRow() && alku.getCol() < loppu.getCol()) {
+				int counter = 1;
+				for (int i = alku.getRow(); i< loppu.getRow(); i++) {
+					if (shakkilauta.isPieceAt(alku.getRow()-counter, alku.getCol()+counter)) {
+						return false;
+					}
+					counter++;
+				}
+			}
+			if (alku.getRow() > loppu.getRow() && alku.getCol() > loppu.getCol()) {
+				int counter = 1;
+				for (int i = alku.getRow(); i< loppu.getRow(); i++) {
+					if (shakkilauta.isPieceAt(alku.getRow()-counter, alku.getCol()-counter)) {
+						return false;
+					}
+					counter++;
+				}
+			}
+			if (alku.getRow() < loppu.getRow() && alku.getCol() > loppu.getCol()) {
+				int counter = 1;
+				for (int i = alku.getRow(); i< loppu.getRow(); i++) {
+					if (shakkilauta.isPieceAt(alku.getRow()+counter, alku.getCol()-counter)) {
+						return false;
+					}
+					counter++;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
+
--- Shakki/src/Shakki/Shakki.java
+++ Shakki/src/Shakki/Shakki.java
@@ -1,20 +1,19 @@
 package Shakki;
 
-import Pelinappulat.*;
+import PeliNappulat.*;
 
 public class Shakki {
-	private Pelilauta shakkilauta;
+	public PeliLauta shakkilauta;
 	
 	public Shakki() {
-		shakkilauta = new Pelilauta();
+		shakkilauta = new PeliLauta();
 		setBoard();
 	}
 	
 	public void setBoard() {
 		for (int i = 0; i < 8; i++) {
-			shakkilauta[1][i] = new Sotilas(1, new Sijainti(1, i));
-			shakkilauta[6][i] = new Sotilas(2, new Sijainti(6, i));
+			shakkilauta.setPieceTo(new Sotilas(1, new Sijainti(1, i)));
+			shakkilauta.setPieceTo(new Sotilas(2, new Sijainti(6, i)));
 		}
-		
 	}
 }
